fdc7d39effa80eddacaa832e1d6cf1c6560ba9c9
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-04-13 23:28:16 -0300

    chore(eslint): added eslint and prettier rules

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
+app.use(routes);
+
 app.listen(3333, () => {
-  console.log("app listening up and to port 3333");
+  console.log('app listening up and to port 3333');
 });
