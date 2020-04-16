6cf630ce1cbee6b6f1ba5782dc2ca114f36ae2d0
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-04-15 21:18:57 -0300

    refactor(dto): added DTO design pattern to transfer objects

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,22 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments: Appointment[] = [];
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+  return res.json(appointments);
+});
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
-
   const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameData = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+
+  const findAppointmentInSameData = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameData) {
@@ -21,9 +25,10 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return res.json(appointment);
 });
