154022652c466245c194d658f141f47c74d69757
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-19 20:35:21 -0300

    feat(models): relation between Appointments & Users

--- src/database/migrations/1587088270086-CreateAppointments.ts
+++ src/database/migrations/1587088270086-CreateAppointments.ts
@@ -8,7 +8,7 @@ class CreateAppointments1587088270086 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
---
+++ src/database/migrations/1587335389473-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,44 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AlterProviderFieldToProviderId1587335389473
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentForeignKey',
+        columnNames: ['provider_id'],
+        referencedTableName: 'users',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentForeignKey');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+      }),
+    );
+  }
+}
