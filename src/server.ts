0e8da0d5345b8bbad6f69189a37b7b72631daf3e
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-20 21:28:56 -0300

    feat(cool emoji): cool emoji

--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('app listening up and to port 3333');
+  console.log('🧨 app listening up and to port 3333');
 });
