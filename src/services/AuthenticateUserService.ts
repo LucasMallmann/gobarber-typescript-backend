e7610de7704053591753844f6a951cebbd50a3cd
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-20 23:32:10 -0300

    feat(jwt): added jwt token on login

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, 'd8077400a91a0aff60bdb87010f53f83', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
