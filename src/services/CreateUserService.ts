71142b93acda73a128f01ce78b101b3fc003005c
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-20 22:20:44 -0300

    feat: debugger config

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,7 +20,13 @@ class CreateUserService {
       throw new Error('Email address already in use!');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
