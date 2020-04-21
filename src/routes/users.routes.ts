ed14eea5630b3db061fb9fcd33300bbda258045f
lucasmallmann <lucasmallmann76@gmail.com>
2020-04-21 13:49:59 -0300

    feat: global error handling middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,18 +12,14 @@ const upload = multer(uploadConfig);
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
-    const user = await createUser.execute({ name, email, password });
+  const createUser = new CreateUserService();
+  const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return response.status(201).json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.status(201).json(user);
 });
 
 usersRouter.patch(
@@ -31,20 +27,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateAvatar = new UpdateUserAvatarService();
+    const updateAvatar = new UpdateUserAvatarService();
 
-      const updatedUser = await updateAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updatedUser = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      delete updatedUser.password;
+    delete updatedUser.password;
 
-      return response.json(updatedUser);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(updatedUser);
   },
 );
 
